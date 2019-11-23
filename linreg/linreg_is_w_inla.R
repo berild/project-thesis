d1877616ab22f82fc01f4dd0978a4f105ba12352
Martin Outzen Berild <martinob@null06.math.ntnu.no>
2019-11-23 14:30:54 +0100

    implement is with inla algorithm from poster

--- linreg/linreg_is_w_inla.R
+++ linreg/linreg_is_w_inla.R
@@ -1,5 +1,6 @@
-library(INLA)
-library(mvtnorm)
+require(INLA)
+require(MASS)
+require(mvtnorm)
 
 rq.beta <- function(x=c(0,0), sigma = diag(5,2,2)) {
   rmvnorm(1,mean=x,sigma = sigma)
@@ -13,163 +14,54 @@ prior.beta <- function(x, sigma = sqrt(1/.001), log = TRUE) {
   sum(dnorm(x, mean = 0, sd= sigma, log = log))
 }
 
-
-fit.inla = function(data, beta){
-  data$oset = data$x%*%beta
-  res = inla(y~1+offset(oset), data = data)
-  return(list(mlik = res$mlik[1],
-              dists = list(intercept = res$marginals.fixed[[1]], 
-                           tau = res$marginals.hyperpar[[1]])))
-}
-
-
-self.norm.imp.samp <- function(marg, post.marg = NA, weight, m.weight){
-  for (i in seq(length(marg))){
-    if(length(post.marg)!=length(marg)){
-      return(marg)
-    }else{
-      tmp.post.marg = post.marg[[i]]
-      tmp.marg = marg[[i]]
-      min.x = min(c(tmp.post.marg[1,1],tmp.marg[1,1]))
-      max.x = max(c(tmp.post.marg[length(tmp.post.marg[,1]),1], tmp.marg[length(tmp.marg[,1]),1]))
-      new.x = seq(min.x,max.x,length.out = 100)
-      new.y = inla.dmarginal(new.x, tmp.post.marg, log = FALSE)
-      # step1 = tmp.post.marg[2,1] - tmp.post.marg[1,1]
-      # step2 = tmp.marg[2,1] - tmp.marg[1,1]
-      # if (step2 < step1){
-      #   new.x = seq(tmp.post.marg[1,1],tmp.post.marg[length(tmp.post.marg[,1]),1],step2)
-      #   new.y = inla.dmarginal(new.x,tmp.post.marg, log = FALSE)
-      #   tmp.post.marg = data.frame(x = new.x,y = new.y)
-      #   step = step2
-      # }else{
-      #   step = step1
-      # }
-      # new.x = tmp.post.marg[,1]
-      # new.y = tmp.post.marg[,2]
-      # if (max(tmp.marg[,1])>max(new.x)){
-      #   new.u = seq(from = max(new.x) + step,
-      #               to = max(tmp.marg[,1])+ step,
-      #               by = step)
-      #   new.x = c(new.x, new.u)
-      #   new.y = c(new.y,rep(0,length(new.u)))
-      # }
-      # if (min(tmp.marg[,1])<min(new.x)){
-      #   new.l = seq(from = min(new.x) - step,
-      #               to = min(tmp.marg[,1]) - step,
-      #               by = - step)
-      #   new.x = c(rev(new.l), new.x)
-      #   new.y = c(rep(0,length(new.l)),new.y)
-      # }
-      tmp.post.marg = data.frame(x = new.x, y = new.y)
-      tmp = inla.dmarginal(tmp.post.marg[,1], tmp.marg, log = FALSE)
-      tmp.post.marg[,2] = tmp*weight/(m.weight + weight) + tmp.post.marg[,2]*m.weight/(m.weight + weight)
-      post.marg[[i]] = tmp.post.marg
-    }
-    return(post.marg)
+calc.theta <- function(theta,weight,eta,i_tot,i_cur){
+  for (i in seq(ncol(eta))){
+    theta$a.mu[i_cur,i] = sum(eta[1:i_tot,i]*weight[1:i_tot])/sum(weight[1:i_tot])
   }
-}
-
-
-calc.var <- function(a.mean,weight,eta){
-  new.var = matrix(NA,ncol = length(a.mean), nrow = length(a.mean))
-  for (i in seq(length(a.mean))){
-    for (j in seq(i,length(a.mean))){
-      new.var[i,j] = new.var[j,i] = sum(weight*weight*(eta[,i]-a.mean[i])*(eta[,j]-a.mean[j]))/(sum(weight))^2
+  for (i in seq(ncol(eta))){
+    for (j in seq(i,ncol(eta))){
+      theta$a.cov[i,j,i_cur] = theta$a.cov[j,i,i_cur] = sum(weight[1:i_tot]*(eta[1:i_tot,i]-theta$a.mu[i_cur,i])*(eta[1:i_tot,j]-theta$a.mu[i_cur,j]))/(sum(weight[1:i_tot]))
     }
   }
-  return(new.var)
+  return(theta)
 }
 
-store.post <- function(marg,margs,j,n.prop){
-  if (length(marg)!=length(margs)){
-    margs = marg
-    for (i in seq(length(marg))){
-      margs[[i]] = list(x = matrix(NA, nrow = n.prop, ncol = length(marg[[i]][,1])),
-                        y = matrix(NA, nrow = n.prop, ncol = length(marg[[i]][,2])))
-      margs[[i]]$x[j,] = marg[[i]][,1]
-      margs[[i]]$y[j,] = marg[[i]][,2]
-    }
-    return(margs)
-  }else{
-    for (i in seq(length(marg))){
-      margs[[i]]$x[j,] = marg[[i]][,1]
-      margs[[i]]$y[j,] = marg[[i]][,2]
-    }
-    return(margs)
-  }
-}
-
-fit.marginals <- function(ws,margs,len = 400){
-  xmin <- min(margs[[1]])
-  xmax <- max(margs[[1]])
-  xx <- seq(xmin, xmax, len = len)
-  tot_ws = sum(ws)
-  marg = numeric(len)
-  for (i in seq(nrow(margs[[1]]))){
-    marg = marg + ws[i]/tot_ws*inla.dmarginal(xx, list(x = margs[[1]][i,], y = margs[[2]][i,]))
-  }
-  data.frame(x = xx, y = marg)
-}
-
-inla.w.is <- function(data, init, prior, d.prop, r.prop, fit.inla, n.prop,target.sd = 1){
-  mlik = numeric(n.prop)
-  eta = matrix(NA, ncol = length(init), nrow = n.prop)
-  weight = numeric(n.prop)
-  a.mean = init
-  a.var = diag(5,length(init),length(init))
-  pb <- txtProgressBar(min = 0, max = n.prop, style = 3)
-  for (i in seq(n.prop)){
+is.w.inla <- function(data, init, prior, d.prop, r.prop, N_0 = 200, N = 400){
+  mlik = numeric(N_0)
+  eta = matrix(NA, ncol = length(init$mu), nrow = N_0)
+  weight = numeric(N_0)
+  theta = list(a.mu = matrix(NA, ncol = length(init$mu), nrow = 2),
+               a.cov = array(NA, dim = c(length(init$mu), length(init$mu), 2))) 
+  theta$a.mu[1,] = init$mu
+  theta$a.cov[,,1] = init$cov
+  pb <- txtProgressBar(min = 0, max = N+N_0, style = 3)
+  for (i in seq(N_0)){
     setTxtProgressBar(pb, i)
-    eta[i,] = r.prop(a.mean, sigma = a.var)
+    eta[i,] = r.prop(theta$a.mu[1,], sigma = theta$a.cov[,,1])
     mod = fit.inla(data,eta[i,])
     mlik[i] = mod$mlik
-    weight[i] = mlik[i] + prior(eta[i,]) - d.prop(y = eta[i,], x = a.mean, sigma = a.var)
+    weight[i] = mlik[i] + prior(eta[i,]) - d.prop(y = eta[i,], x = theta$a.mu[1,], sigma = theta$a.cov[,,1])
   }
   weight = exp(weight - max(weight))
-  a.mean = c(sum(eta[,1]*weight)/sum(weight),sum(eta[,2]*weight)/sum(weight))
+  theta = calc.theta(theta,weight,eta,nrow(eta),2)
   margs = NA
-  a.var = calc.var(a.mean,weight,eta)
-  cat("Set mean =",a.mean,"; var =",a.var,"\nSampling...")
-  eta = matrix(NA, ncol = length(init), nrow = n.prop)
-  weight = numeric(n.prop)
-  mlik = numeric(n.prop)
-  for (i in seq(n.prop)){
-    setTxtProgressBar(pb, i)
-    eta[i,] = r.prop(a.mean, sigma = a.var)
+  eta = matrix(NA, ncol = length(init$mu), nrow = N)
+  weight = numeric(N)
+  mlik = numeric(N)
+  for (i in seq(N)){
+    setTxtProgressBar(pb, i+N_0)
+    eta[i,] = r.prop(theta$a.mu[2,], sigma = theta$a.cov[,,2])
     mod = fit.inla(data,eta[i,])
-    margs = store.post(mod$dists,margs,i,n.prop)
+    margs = store.post(mod$dists,margs,i,N)
     mlik[i] = mod$mlik
-    weight[i] = mlik[i] + prior(eta[i,]) - d.prop(eta[i,], a.mean, sigma = a.var)
+    weight[i] = mlik[i] + prior(eta[i,]) - d.prop(y = eta[i,], x = theta$a.mu[2,], sigma = theta$a.cov[,,2])
   }
   weight = exp(weight - max(weight))
+  eta_kern = kde2d.weighted(x = eta[,1], y = eta[,2], w = weight/(sum(weight)), n = 100, lims = c(1,3,-3,-1))
+  eta_kern = data.frame(expand.grid(x=eta_kern$x, y=eta_kern$y), z=as.vector(eta_kern$z))
   return(list(eta = eta,
-              margs = margs,
+              theta = theta,
+              eta_kern = eta_kern,
+              margs = lapply(margs, function(x){fit.marginals(weight,x)}),
               weight = weight))
 }
-
-
-sample.linreg <- function(){
-  n = 100
-  x1 = runif(n)
-  x2 = runif(n)
-  err = rnorm(n)
-  y = 3 + 2*x1 -2*x2 + err
-  return(list(y = y,x = matrix(c(x1,x2),ncol = 2)))
-}
-
-set.seed(1)
-df = sample.linreg()
-mod = inla.w.is(df,init = c(0,0), prior.beta, dq.beta, rq.beta, fit.inla, n.prop = 100)
-
-fit.marginals <- function(ws,margs,len = 100){
-  xmin <- min(margs[[1]])
-  xmax <- max(margs[[1]])
-  xx <- seq(xmin, xmax, len = len)
-  marg = numeric(len)
-  for (i in seq(nrow(margs[[1]]))){
-    marg = marg + inla.dmarginal(xx, list(x = margs[[1]][i,], y = margs[[2]][i,]))
-  }
-  data.frame(x = xx, y = marg)
-}
-
-
