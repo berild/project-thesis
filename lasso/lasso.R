3850a462d99b6a3bc370327601af3065d08fdde2
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2019-11-28 12:48:26 +0100

    change to Rscript for running on cluster

--- lasso/lasso.R
+++ lasso/lasso.R
@@ -1,124 +1,147 @@
+# loading required packages
 library(INLA)
-library(tidyverse)
 library(ISLR)
 library(glmnet)
 library(smoothmest)
 library(mvtnorm)
-
+library(parallel)
+library(coda)
 
 data(Hitters)
+summary(Hitters)
 
+#Check NA's and fix
+sum(is.na(Hitters$Salary))
 Hitters <- na.omit(Hitters)
 
+#
+# The Lasso
+#
+
+#Create variables for lasso
 x <- model.matrix(Salary ~ ., Hitters)[, -1]
-x <- x[, 1:5]
-y <- Hitters$Salary
-
-
-y <- scale(y)
+x <- x[, 1:5] #Just for testing
 x <- scale(x)
-d <- list(y = y, x = x)
-n.beta <- ncol(d$x)
+y <- Hitters$Salary
+y <- scale(y)
+df <- list(y = y, x = x)
+n.beta <- ncol(df$x)
+
+# ml estimates
+ml = summary(lm(y~x, data = df))$coefficients[2:6,1:2]
+
+#Indices for train/test model
+set.seed(1)
+train <- sample(1:nrow(x), nrow(x)/2)
+test <- (-train)
+
+
+#Grid for lambda parameter in lasso
+grid <- 10^seq(10, -2, length = 100)
+
+#Fit lasso model for several values of lambda
+lasso.mod <- glmnet(x[train, ] , y[train], alpha = 1, lambda = grid)
+plot(lasso.mod)
+
+#CV
+set.seed(1)
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 1)
+plot(cv.out)
+
+#Take best lambda for lasso model
+bestlam <- cv.out$lambda.min
+
+#Predcit with lasso on test data
+lasso.pred <- predict(lasso.mod, s = bestlam, newx = x[test, ])
+mean((lasso.pred - y[test])^2)
+
+#Fit model to complete dataset
+out <- glmnet(x, y, alpha = 1, lambda = grid)
+lasso.coef <- predict(out, type = "coefficients", s = bestlam)
+
+#Check estimated coefficients
+lasso.coef
+lasso.coef[lasso.coef != 0]
+
+#Fitted values
+lasso.fitted <- predict(out, s = bestlam, newx = x)
+# importing dataset
 
 # finding inverse of the precision
 stdev.samp <- .25 * solve(t(x)%*%x)
 
+source("./lasso/lasso_general_function.R")
 
-fit.inla <- function(data, b) {
-  data$oset = data$x %*% b
-  res = inla(y ~ -1 + offset(oset), data = data)
-  res = inla.rerun(res)
-  return(list(mlik = res$mlik[[1]],  
-              dists = list(tau = res$marginals.hyperpar[[1]])))
-}
-
-prior.beta <- function(x, mu = 0, lambda = 0.073, log = TRUE) {
-  res <- sum(log(ddoublex(x, mu = mu, lambda = lambda)))
-  
-  if(!log) { res <- exp(res) }
-  
-  return(res)
-}
-
-dq.beta <- function(x, y, sigma = stdev.samp, log =TRUE) {
-  dmvnorm(y, mean = x, sigma = sigma, log = log)
-}
-
-rq.beta <- function(x, sigma = stdev.samp) {
-  as.vector(rmvnorm(1, mean = x, sigma = sigma))
-}
+source("./lasso/lasso_ml_w_inla.R")
+is_w_inla_mod = ml.w.inla(data = df, init = list(mu = rep(0,n.beta), cov = 6*stdev.samp), prior.beta, dq.beta, rq.beta, fit.inla, N_t = rep(20,20))
+save(mod, file = "./lasso/lasso-ml-w-inla.Rdata")
 
 
-moving.marginals <- function(marg, post.marg, n){
-  for (i in seq(length(post.marg))){
-    tmp.post.marg = post.marg[[i]]
-    tmp.marg = marg[[i]]
-    step = tmp.post.marg[2,1] - tmp.post.marg[1,1]
-    new.x = tmp.post.marg[,1]
-    new.y = tmp.post.marg[,2]
-    if (max(tmp.marg[,1])>max(new.x)){
-      new.u = seq(from = max(new.x) + step,
-                  to = max(tmp.marg[,1])+ step,
-                  by = step)
-      new.x = c(new.x, new.u)
-      new.y = c(new.y,rep(0,length(new.u)))
-    }
-    if (min(tmp.marg[,1])<min(new.x)){
-      new.l = seq(from = min(new.x) - step,
-                  to = min(tmp.marg[,1]) - step,
-                  by = - step)
-      new.x = c(rev(new.l), new.x)
-      new.y = c(rep(0,length(new.l)),new.y)
-    }
-    tmp.post.marg = data.frame(x = new.x, y = new.y)
-    tmp = inla.dmarginal(tmp.post.marg[,1], tmp.marg, log = FALSE)
-    tmp.post.marg[,2] = (tmp + (n-1)*tmp.post.marg[,2])/n
-    post.marg[[i]] = tmp.post.marg
-  }
-  post.marg
-}
+source("./lasso/lasso_pp_w_inla.R")
+pp_w_inla_mod = pp.w.inla(data = df, init = list(mu = rep(0,n.beta), cov = 6*stdev.samp), prior.beta, dq.beta, rq.beta, fit.inla, N_t = rep(20,20))
+save(mod, file = "./lasso/lasso-pp-w-inla.Rdata")
 
 
-lasso.mcmc.w.inla <- function(data, n.beta, stdev.samp, n.samples = 100, n.burnin = 5, n.thin = 1){
-  beta = matrix(data = NA,nrow = n.samples, ncol = n.beta)
-  colnames(beta) = colnames(data$x)
-  mlik = numeric(n.samples)
-  acc.vec = numeric(n.samples)
-  beta[1,] = rep(0,n.beta)
-  mod.curr = fit.inla(data, beta[1,])
-  mlik[1] = mod.curr$mlik
-  pb <- txtProgressBar(min = 0, max = n.samples, style = 3)
-  for (i in seq(2,n.samples)){
-    setTxtProgressBar(pb, i)
-    beta.new = rq.beta(beta[i-1,],sigma = stdev.samp)
-    mod.new = fit.inla(data,beta.new)
-    lacc1 = mod.new$mlik + prior.beta(beta.new) + dq.beta(beta.new, beta[i-1,])
-    lacc2 = mod.curr$mlik + prior.beta(beta[i-1,]) + dq.beta(beta[i-1,], beta.new)
-    acc = min(1,exp(lacc1 - lacc2))
-    if (runif(1) < acc){
-      beta[i,] = beta.new
-      mod.curr = mod.new
-      mlik[i] = mod.new$mlik
-      acc.vec[i] = T
-    }else{ 
-      beta[i,] = beta[i-1,]
-      mlik[i] = mlik[i-1]
-      acc.vec[i] = F
-    }
-    if(i == n.burnin){
-      post.marg = mod.curr$dists
-    }else if(i > n.burnin){
-      post.marg = moving.marginals(mod.curr$dists,
-                                   post.marg,
-                                   i-n.burnin+1)
-    }
-  }
-  return(list(beta = beta, 
-              post.marg = post.marg,
-              acc.vec = acc.vec,
-              mlik = mlik))
-}
+source("./lasso/lasso_is_w_inla.R")
+is_w_inla_mod = is.w.inla(data = df, init = list(mu = rep(0,n.beta), cov = 6*stdev.samp), prior.beta, dq.beta, rq.beta, fit.inla, N_t = rep(20,20))
+save(mod, file = "./lasso/lasso-amis-w-inla.Rdata")
 
-set.seed(123)
-mod = lasso.mcmc.w.inla(d, n.beta, stdev.samp, n.samples = 100000, n.burnin = 500)
+
+source("./lasso/lasso_amis_w_inla.R")
+amis_w_inla_mod = amis.w.inla(data = df, init = list(mu = rep(0,n.beta), cov = 6*stdev.samp), prior.beta, dq.beta, rq.beta, fit.inla, N_t = rep(20,20))
+save(amis_w_inla_mod, file = "./lasso/lasso-amis-w-inla.Rdata")
+
+
+source("./lasso/lasso_mcmc_w_inla.Rdata")
+mcmc_w_inla_mod = mcmc.w.inla(data = df, init = list(mu = rep(0,n.beta), cov = 6*stdev.samp), prior.beta, dq.beta, rq.beta, fit.inla, N_t = rep(20,20))
 save(mod, file = "./lasso/lasso-mcmc-w-inla.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# moving.marginals <- function(marg, post.marg, n){
+#   for (i in seq(length(post.marg))){
+#     tmp.post.marg = post.marg[[i]]
+#     tmp.marg = marg[[i]]
+#     step = tmp.post.marg[2,1] - tmp.post.marg[1,1]
+#     new.x = tmp.post.marg[,1]
+#     new.y = tmp.post.marg[,2]
+#     if (max(tmp.marg[,1])>max(new.x)){
+#       new.u = seq(from = max(new.x) + step,
+#                   to = max(tmp.marg[,1])+ step,
+#                   by = step)
+#       new.x = c(new.x, new.u)
+#       new.y = c(new.y,rep(0,length(new.u)))
+#     }
+#     if (min(tmp.marg[,1])<min(new.x)){
+#       new.l = seq(from = min(new.x) - step,
+#                   to = min(tmp.marg[,1]) - step,
+#                   by = - step)
+#       new.x = c(rev(new.l), new.x)
+#       new.y = c(rep(0,length(new.l)),new.y)
+#     }
+#     tmp.post.marg = data.frame(x = new.x, y = new.y)
+#     tmp = inla.dmarginal(tmp.post.marg[,1], tmp.marg, log = FALSE)
+#     tmp.post.marg[,2] = (tmp + (n-1)*tmp.post.marg[,2])/n
+#     post.marg[[i]] = tmp.post.marg
+#   }
+#   post.marg
+# }
+
