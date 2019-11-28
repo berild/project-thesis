4ca6130b34a56f414757082cdd1538b7429e5101
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2019-11-28 12:50:03 +0100

    update plot script with new structure

--- lasso/plot_lasso.R
+++ lasso/plot_lasso.R
@@ -1,56 +1,15 @@
-# McMC with INLA results
-tau = mod$tau
+library(ggplot2)
 
-ggplot(tau) + 
-  geom_line(aes(x= x, y = y))
+## loading simulation results
+load(file = "./lasso/sims/lasso-inla.Rdata")
+load(file = "./lasso/sims/lasso-ml-w-inla.Rdata")
+load(file = "./lasso/sims/lasso-pp-w-inla.Rdata")
+load(file = "./lasso/sims/lasso-is-w-inla.Rdata")
+load(file = "./lasso/sims/lasso-amis-w-inla.Rdata")
+load(file = "./lasso/sims/lasso-mcmc-w-inla.Rdata")
 
-res = cbind(data.frame(step = seq(nrow(mod$beta))),as.data.frame(mod$beta))
-res$is_burnin = c(rep(T,5),rep(F,nrow(res)-5))
 
-params = colnames(res[,-c(1,ncol(res))])
-
-means = data.frame(key = params, 
-                   value = c(sapply(res[res$is_burnin==F,params],mean)))
-
-res = gather(res,key,value,params)
-
-traceplot <- ggplot(res, aes(x = step, y = value, color = is_burnin)) +
-  geom_line() +
-  facet_wrap(vars(key),scales="free",ncol = 2)
-
-traceplot
-
-distplot <- ggplot(res[res$is_burnin==F,]) +
-  geom_density(aes(x = value),fill = "deepskyblue" , alpha = 0.5) +
-  geom_vline(data = means,aes(xintercept = value),color = "firebrick") + 
-  geom_text(data = means,aes(label = sprintf("%.3f",value), x = value), 
-            y = 0.2, size = 6,color = "firebrick") + 
-  facet_wrap(vars(key),scales="free",ncol = 2)
-
-distplot
-
-# McMC results
-res = cbind(data.frame(step = seq(nrow(mod$beta)),acc.prob = mod$acc.prob, tau = mod$tau), as.data.frame(mod$beta))
-res$is_burnin = c(rep(T,500),rep(F,nrow(res)-500))
-params = colnames(res[,-c(1,2,9)])
-means = data.frame(key = params, 
-                   value = c(sapply(res[,params],mean)))
-
-res = gather(res,key,value,params)
-
-traceplot <- ggplot(res, aes(x = step, y = value, color = is_burnin)) +
-  geom_line() +
-  facet_wrap(vars(key),scales="free",ncol = 2)
-
-traceplot
-
-distplot <- ggplot(res[res$is_burnin==F,]) +
-  geom_density(aes(x = value),fill = "deepskyblue" , alpha = 0.5) +
-  geom_vline(data = means,aes(xintercept = value),color = "firebrick") + 
-  geom_text(data = means,aes(label = sprintf("%.3f",value), x = value), 
-            y = 0.2, size = 6,color = "firebrick") + 
-  facet_wrap(vars(key),scales="free",ncol = 2)
-
-distplot
-
-                   
+ggplot() + 
+  geom_line(data = amis_w_inla_mod$margs$tau, aes(x = x, y = y, color = "AMIS with INLA"))+
+  labs(color = "") + 
+  theme_bw()
