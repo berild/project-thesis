109727cf030dcaa4d9acf2bd419e2b02a04912fb
Martin Outzen Berild <martinob@null05.math.ntnu.no>
2019-11-28 11:14:00 +0100

    fix plot function packages

--- linreg/plot_linreg.R
+++ linreg/plot_linreg.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 ## loading simulation results
 load(file = "./linreg/sims/linreg-inla.Rdata")
 load(file = "./linreg/sims/linreg-ml-w-inla.Rdata")
@@ -10,12 +11,13 @@ load(file = "./linreg/sims/linreg-mcmc-w-inla.Rdata")
 ## Plotting univariate distribution of the intercept; alpha
 
 ggplot() +
-  geom_vline(xintercept = ml[1,1]) +  
+  #geom_vline(xintercept = ml[1,1]) +  
   geom_line(data = ml_w_inla_mod$dists$intercept, aes(x = x, y = y, color = "ML with INLA")) + 
   geom_line(data = data.frame(inla_mod$marginals.fixed[[1]]), aes(x = x, y = y, color = "INLA")) + 
   geom_line(data = pp_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "PP with INLA")) + 
   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "AMIS with INLA")) + 
   geom_line(data = is_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "IS with INLA")) + 
+  geom_line(data = mcmc_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "MCMC with INLA")) + 
   labs(color = "") + 
   theme_bw()
 
@@ -27,16 +29,18 @@ ggplot() +
   geom_line(data = pp_w_inla_mod$margs$tau, aes(x = x, y = y, color = "PP with INLA")) + 
   geom_line(data = amis_w_inla_mod$margs$tau, aes(x = x, y = y, color = "AMIS with INLA")) + 
   geom_line(data = is_w_inla_mod$margs$tau, aes(x = x, y = y, color = "IS with INLA")) +
+  geom_line(data = mcmc_w_inla_mod$margs$tau, aes(x = x, y = y, color = "MCMC with INLA")) + 
   labs(color = "") + 
   theme_bw()
 
 ## Plotting bivariate distribution of model params; betas'
 
 ggplot() + 
-  geom_point(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), shape = 4,size = 3, color = "navyblue") + 
-  geom_text(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), label="ML", vjust=2) + 
+  #geom_point(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), shape = 4,size = 3, color = "navyblue") + 
+  #geom_text(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), label="ML", vjust=2) + 
   geom_contour(data = pp_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "PP with INLA")) + 
   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA")) + 
-  geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA")) + 
+  #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA")) + 
+  #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA")) + 
   labs(color = "") + 
   theme_bw()
