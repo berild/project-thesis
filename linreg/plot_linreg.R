d8cd3e61e91ac31d6112a94976b132d34859aeca
Berild <moutzebe@gmail.com>
2019-11-28 11:19:04 +0100

    fix figure creation script

--- linreg/plot_linreg.R
+++ linreg/plot_linreg.R
@@ -11,7 +11,7 @@ load(file = "./linreg/sims/linreg-mcmc-w-inla.Rdata")
 ## Plotting univariate distribution of the intercept; alpha
 
 ggplot() +
-  #geom_vline(xintercept = ml[1,1]) +  
+  geom_vline(xintercept = ml[1,1]) +  
   geom_line(data = ml_w_inla_mod$dists$intercept, aes(x = x, y = y, color = "ML with INLA")) + 
   geom_line(data = data.frame(inla_mod$marginals.fixed[[1]]), aes(x = x, y = y, color = "INLA")) + 
   geom_line(data = pp_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "PP with INLA")) + 
@@ -36,11 +36,11 @@ ggplot() +
 ## Plotting bivariate distribution of model params; betas'
 
 ggplot() + 
-  #geom_point(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), shape = 4,size = 3, color = "navyblue") + 
-  #geom_text(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), label="ML", vjust=2) + 
+  geom_point(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), shape = 4,size = 3, color = "navyblue") + 
+  geom_text(data = data.frame(x = ml[2,1], y = ml[3,1]), aes(x = x, y = y), label="ML", vjust=2) + 
   geom_contour(data = pp_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "PP with INLA")) + 
   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA")) + 
-  #geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA")) + 
-  #geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA")) + 
+  geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA")) + 
+  geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA")) + 
   labs(color = "") + 
   theme_bw()
