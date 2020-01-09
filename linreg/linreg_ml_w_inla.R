9aee02c7cd0c456465d524eb0caf84ba1dfb2db5
Berild <moutzebe@gmail.com>
2020-01-09 19:58:23 +0100

    remove files unused files

--- linreg/linreg_ml_w_inla.R
+++
@@ -1,8 +0,0 @@
-require(INLA)
-fit.inla.ml <- function(data,ml){
-  data$oset = data$x%*%ml[-1,1]
-  res = inla(y~offset(oset), data = data)
-  return(list(mlik = res$mlik[1],
-              dists = list(intercept = data.frame(res$marginals.fixed[[1]]), 
-                           tau = data.frame(res$marginals.hyperpar[[1]]))))
-}
