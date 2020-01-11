d8f1f9e179220a1e64ee85589c0cf146d118bd6f
Berild <moutzebe@gmail.com>
2020-01-11 22:14:44 +0100

    try for crash fix inla

--- missing/missing_mcmc_w_inla.R
+++ missing/missing_mcmc_w_inla.R
@@ -1,7 +1,7 @@
 require(INLA)
 
 
-dq.x.mis <- function(x, y, sigma = sqrt(1/0.001), log =TRUE) {
+dq.x.mis <- function(x, y, sigma = sqrt(1/0.1), log =TRUE) {
   res <- dnorm(y, mean = x, sd = sigma, log = log)
   
   if(log) {
@@ -11,7 +11,7 @@ dq.x.mis <- function(x, y, sigma = sqrt(1/0.001), log =TRUE) {
   }
 }
 
-rq.x.mis <- function(x, sigma = sqrt(1/0.001)) {
+rq.x.mis <- function(x, sigma = sqrt(1/0.1)) {
   rnorm(length(x), mean = x, sd = sigma)
 }
 
