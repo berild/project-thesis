bb608a9b8bc4135b3d88ee5368165353cc23b189
Martin Outzen Berild <martinob@markov.math.ntnu.no>
2020-01-15 13:22:51 +0100

    change size of plots

--- linreg/plot_linreg.R
+++ linreg/plot_linreg.R
@@ -4,8 +4,6 @@ library(ggpubr)
 
 ## loading simulation results
 load(file = "./linreg/sims/linreg-inla.Rdata")
-#load(file = "./linreg/sims/linreg-ml-w-inla.Rdata")
-#load(file = "./linreg/sims/linreg-pp-w-inla.Rdata")
 load(file = "./linreg/sims/linreg-is-w-inla.Rdata")
 load(file = "./linreg/sims/linreg-amis-w-inla.Rdata")
 load(file = "./linreg/sims/linreg-mcmc-w-inla.Rdata")
@@ -15,10 +13,7 @@ height = 5
 ## Plotting univariate distribution of the intercept; alpha
 
 p1 <-ggplot() +
-  #geom_vline(xintercept = ml[1,1]) +  
-  #geom_line(data = ml_w_inla_mod$dists$intercept, aes(x = x, y = y, color = "ML with INLA")) + 
   geom_line(data = data.frame(inla_mod$marginals.fixed[[1]]), aes(x = x, y = y)) + 
-  #geom_line(data = pp_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "PP with INLA")) + 
   geom_line(data = amis_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "AMIS with INLA")) + 
   geom_line(data = is_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "IS with INLA")) + 
   geom_line(data = mcmc_w_inla_mod$margs$intercept, aes(x = x, y = y, color = "MCMC with INLA")) + 
@@ -32,9 +27,7 @@ ggsave(filename = "linreg_intercept_plot.pdf", plot = p1, device = NULL, path = 
 ## Plotting univariate distribution of the precition; tau
 
 p2 <- ggplot() + 
-  #geom_line(data = ml_w_inla_mod$dists$tau, aes(x = x, y = y, color = "ML with INLA")) + 
   geom_line(data = data.frame(inla_mod$marginals.hyperpar[[1]]), aes(x = x, y = y)) + 
-  #geom_line(data = pp_w_inla_mod$margs$tau, aes(x = x, y = y, color = "PP with INLA")) + 
   geom_line(data = amis_w_inla_mod$margs$tau, aes(x = x, y = y, color = "AMIS with INLA")) + 
   geom_line(data = is_w_inla_mod$margs$tau, aes(x = x, y = y, color = "IS with INLA")) +
   geom_line(data = mcmc_w_inla_mod$margs$tau, aes(x = x, y = y, color = "MCMC with INLA")) + 
@@ -49,7 +42,6 @@ ggsave(filename = "linreg_tau_plot.pdf", plot = p2, device = NULL, path = "./lin
 p3 <- ggplot() + 
   geom_point(data = data.frame(x = 2, y = -2), aes(x = x, y = y), shape = 4,size = 3) + 
   geom_text(data = data.frame(x = 2, y = -2), aes(x = x, y = y), label="True Value", vjust=2) + 
-  #geom_contour(data = pp_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "PP with INLA")) + 
   geom_contour(data = amis_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "AMIS with INLA"),bins = 6) + 
   geom_contour(data = is_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "IS with INLA"),bins = 6) + 
   geom_contour(data = mcmc_w_inla_mod$eta_kern, aes(x = x, y = y, z = z, color = "MCMC with INLA"),bins = 6) + 
@@ -60,8 +52,8 @@ p3
 ggsave(filename = "linreg_contour_plot.pdf", plot = p3, device = NULL, path = "./linreg/figures/",
        scale = 1, width = width, height = height, units = "in", dpi=5000)
 
-amis_w_inla_mod$ess = running.ESS(amis_w_inla_mod$eta, amis_w_inla_mod$times,ws =  amis_w_inla_mod$weight/sum(amis_w_inla_mod$weight))
-is_w_inla_mod$ess = running.ESS(is_w_inla_mod$eta, is_w_inla_mod$times,ws =  is_w_inla_mod$weight/sum(is_w_inla_mod$weight))
+amis_w_inla_mod$ess = running.ESS(amis_w_inla_mod$eta, amis_w_inla_mod$times,ws =  amis_w_inla_mod$weight)
+is_w_inla_mod$ess = running.ESS(is_w_inla_mod$eta, is_w_inla_mod$times,ws =  is_w_inla_mod$weight)
 mcmc_w_inla_mod$ess = running.ESS(mcmc_w_inla_mod$eta,mcmc_w_inla_mod$times)
 
 p4 <- ggplot() + 
@@ -72,7 +64,7 @@ p4 <- ggplot() +
   scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   annotation_logticks() + 
-  labs(color = "",x="Runtime (secs)",y="Effective sample size") + 
+  labs(color = "",x="Runtime (sec)",y="Effective sample size") + 
   coord_cartesian(xlim=c(min(amis_w_inla_mod$ess$time),max(mcmc_w_inla_mod$ess$time))) + 
   theme_bw() + 
   theme(legend.position="bottom")
@@ -82,51 +74,44 @@ ggsave(filename = "linreg_ess_plot.pdf", plot = p4, device = NULL, path = "./lin
        scale = 1, width = width, height = height, units = "in", dpi=5000)
 
 
-multi_to_uni_kde <- function(multi_kern){
-  seq1 = seq(min(multi_kern$x),max(multi_kern$x),length.out = 51)
-  seq2 = seq(min(multi_kern$y),max(multi_kern$y),length.out = 51)
-  uni_kern1 = data.frame(x = seq1[-51] + 1/2*(seq1[2]-seq1[1]), 
-                        y = rep(0,50))
-  uni_kern2 = data.frame(x = seq2[-51] + 1/2*(seq2[2]-seq2[1]), 
-                        y = rep(0,50))
-  int1 = int2 = 0
-  for (i in seq(2,51)){
-    uni_kern1$y[i-1] = mean(multi_kern$z[(seq1[i-1]<=multi_kern$x)&(seq1[i]>multi_kern$x)])
-    int1 = int1 + uni_kern1$y[i-1]*(seq1[i]-seq1[i-1])
-    uni_kern2$y[i-1] = mean(multi_kern$z[(seq2[i-1]<=multi_kern$y)&(seq2[i]>multi_kern$y)])
-    int2 = int2 + uni_kern2$y[i-1]*(seq2[i]-seq2[i-1])
-  }
-  uni_kern1$y = uni_kern1$y/int1
-  uni_kern2$y = uni_kern2$y/int2
-  return(list(kern1 = uni_kern1,
-              kern2 = uni_kern2))
-}
-
-amis_uni_kerns = multi_to_uni_kde(amis_w_inla_mod$eta_kern)
-is_uni_kerns = multi_to_uni_kde(is_w_inla_mod$eta_kern)
-mcmc_uni_kerns = multi_to_uni_kde(mcmc_w_inla_mod$eta_kern)
+amis_kerns = lapply(seq(ncol(amis_w_inla_mod$eta)), function(x){
+  as.data.frame(density(x = amis_w_inla_mod$eta[,x],
+                        weights = amis_w_inla_mod$weight/sum(amis_w_inla_mod$weight),
+                        kernel = "gaussian")[c(1,2)])
+})
+is_kerns = lapply(seq(ncol(is_w_inla_mod$eta)), function(x){
+  as.data.frame(density(x = is_w_inla_mod$eta[,x],
+                        weights = is_w_inla_mod$weight/sum(is_w_inla_mod$weight),
+                        kernel = "gaussian")[c(1,2)])
+})
+mcmc_kerns = lapply(seq(ncol(mcmc_w_inla_mod$eta)), function(x){
+  as.data.frame(density(x = mcmc_w_inla_mod$eta[,x],
+                        kernel = "gaussian")[c(1,2)])
+})
 
 p5 <- ggplot() + 
-  geom_line(data = as.data.frame(inla_mod$marginals.fixed$x1), aes(x=x,y=y)) +
-  geom_line(data = amis_uni_kerns$kern1, aes(x=x,y=y,color = "AMIS with INLA")) + 
-  geom_line(data = is_uni_kerns$kern1, aes(x=x,y=y,color = "IS with INLA")) + 
-  geom_line(data = mcmc_uni_kerns$kern1, aes(x=x,y=y, color = "MCMC with INLA")) + 
+  geom_line(data = as.data.frame(inla_mod$marginals.fixed$x1), aes(x=x,y=y)) + 
+  geom_line(data = amis_kerns[[1]], aes(x=x,y=y,color="AMIS with INLA")) +
+  geom_line(data= is_kerns[[1]], aes(x=x,y=y,color="IS with INLA")) + 
+  geom_line(data= mcmc_kerns[[1]], aes(x=x,y=y,color="MCMC with INLA")) + 
   coord_cartesian(xlim = c(0.5,3.5)) + 
   labs(color = "", x = "",y="",title=expression(beta[1]))+
   theme_bw() + 
   theme(legend.position="bottom",plot.title = element_text(hjust = 0.5))
+p5
 ggsave(filename = "linreg_beta1_plot.pdf", plot = p5, device = NULL, path = "./linreg/figures/",
        scale = 1, width = width, height = height, units = "in", dpi=5000)
 
 p6 <- ggplot() + 
   geom_line(data = as.data.frame(inla_mod$marginals.fixed$x2), aes(x=x,y=y)) + 
-  geom_line(data = amis_uni_kerns$kern2, aes(x=x,y=y, color = "AMIS with INLA")) + 
-  geom_line(data = is_uni_kerns$kern2, aes(x=x,y=y, color = "IS with INLA")) + 
-  geom_line(data = mcmc_uni_kerns$kern2, aes(x=x,y=y, color = "MCMC with INLA")) + 
+  geom_line(data = amis_kerns[[2]], aes(x=x,y=y,color="AMIS with INLA")) +
+  geom_line(data= is_kerns[[2]], aes(x=x,y=y,color="IS with INLA")) + 
+  geom_line(data= mcmc_kerns[[2]], aes(x=x,y=y,color="MCMC with INLA")) + 
   labs(color = "", x = "",y="",title = expression(beta[2]))+
   coord_cartesian(xlim = c(-3.5,-0.5))  +
   theme_bw() + 
   theme(legend.position="bottom",plot.title = element_text(hjust = 0.5))
+p6
 ggsave(filename = "linreg_beta2_plot.pdf", plot = p6, device = NULL, path = "./linreg/figures/",
        scale = 1, width = width, height = height, units = "in", dpi=5000)
 
@@ -134,4 +119,4 @@ ggsave(filename = "linreg_beta2_plot.pdf", plot = p6, device = NULL, path = "./l
 ptot <- ggarrange(p3, p4, p5, p6, p2, p1, ncol=2, nrow=3, common.legend = TRUE, legend="bottom")
 ptot
 ggsave(filename = "linreg_tot.pdf", plot = ptot, device = NULL, path = "./linreg/figures/",
-       scale = 1, width = 2*width, height = 3*height, units = "in", dpi=5000)
+       scale = 1, width = 2*width, height = 2*height, units = "in", dpi=5000)
