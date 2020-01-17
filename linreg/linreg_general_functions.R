239df17d29c6a69149909ee14e9de9cece15f285
Martin Outzen Berild <martinob@markov.math.ntnu.no>
2020-01-17 10:24:21 +0100

    fix ess calculation issue

--- linreg/linreg_general_functions.R
+++ linreg/linreg_general_functions.R
@@ -91,8 +91,8 @@ running.ESS <- function(eta, times, ws = NA, norm = TRUE,step = 100){
       sum(ws[1:x])^2/(sum(ws[1:x]^2))
     }))
   }
-  ess.df = data.frame(time = times[seq(1,length(times),step)],
-                      ess = ess[seq(1,length(times),step)])
+  ess.df = data.frame(time = c(times[1],times[seq(step,length(times),step)]),
+                      ess = c(ess[1],ess[seq(step,length(times),step)]))
   return(ess.df)
 }
 
