01b8396b4e86d01bf063aeeede782f5d5ead9ed0
berild <berild@github.com>
2020-01-19 01:54:12 +0100

    fix sample size issue

--- lasso/lasso_amis_w_inla.R
+++ lasso/lasso_amis_w_inla.R
@@ -60,7 +60,6 @@ amis.w.inla <- function(data, init, prior, d.prop, r.prop, fit.inla, N_t = rep(2
   }else{
     ncores = detectCores()
   }
-  N_0 = round(sum(N_t)/2)
   N_tot = N_0 + sum(N_t)
   mlik = numeric(N_tot)
   eta = matrix(NA, ncol = length(init$mu), nrow = N_tot)
